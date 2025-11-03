978940bff7b2c90a30f292d5dfa65938df520b0a
Daniel Rosas Mialma <dan11_daniel@hotmail.com>
2025-11-03 09:21:09 -0600

    Correcion en Retorno de Controllers

--- Product/Product/Internals/Controllers/IDesactiveProductController.cs
+++ Product/Product/Internals/Controllers/IDesactiveProductController.cs
@@ -1,14 +1,9 @@
 ﻿using Product.Dtos;
 using Product.Dtos.ValueObjects;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Product.Internals.Controllers;
 
 internal interface IDesactiveProductController
 {
-    Task<HandlerRequestResult<ProductDto>> DesactiveProductAsync(DesactivateProductDto desactivateProductDto);
+    Task<HandlerRequestResult> DesactiveProductAsync(DesactivateProductDto desactivateProductDto);
 }
--- Product/Product/Internals/Controllers/IUpdateProductController.cs
+++ Product/Product/Internals/Controllers/IUpdateProductController.cs
@@ -1,14 +1,9 @@
 ﻿using Product.Dtos;
 using Product.Dtos.ValueObjects;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Product.Internals.Controllers;
 
 internal interface IUpdateProductController
 {
-    Task<HandlerRequestResult<ProductDto>> UpdateProductAsync(UpdateProductDto updateProductDto);
+    Task<HandlerRequestResult> UpdateProductAsync(UpdateProductDto updateProductDto);
 }
